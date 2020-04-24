a8caf67ff406b424cb0abf522dd48d03a8eb6abb
Eduardo Ferreira <eduardo.sni.ferreira@gmail.com>
2020-04-24 14:18:29 -0300

    Create a process manager to enable multiples bots

---
+++ Process.py
@@ -0,0 +1,42 @@
+import signal
+import subprocess
+import os
+
+class Process:
+    def __init__(self):
+        self.names = []
+        self.process = []
+
+    def listProcess(self):
+        return self.names
+
+    def getpid(self, name):
+        return os.getpgid(self.process[self.names.index(name)].pid)
+
+    def add(self, command, name, cwd, virtual_machine = 0):
+        if virtual_machine:
+            self.process.append(
+                subprocess.Popen(command, stdout=subprocess.PIPE, stdin=subprocess.PIPE, shell=True, preexec_fn=os.setsid,cwd=cwd))
+            self.names.append(name)
+        else:
+            self.process.append(subprocess.Popen('su eduardo', stdout=subprocess.PIPE, stdin=subprocess.PIPE, shell=True, preexec_fn=os.setsid,cwd=cwd))
+            self.process[-1].stdin.write('source env/bin/activate\n'.encode('utf-8'))
+            command_string = '{} &\n'.format(command)
+            self.process[-1].stdin.write(command_string.encode('utf-8'))
+            self.process[-1].stdin.close()
+            self.names.append(name)
+
+    def kill(self, name):
+        try:
+            os.killpg(os.getpgid(self.process[self.names.index(name)].pid), signal.SIGTERM)
+            print(self.process[self.names.index(name)].pid)
+            self.process.remove(self.process[self.names.index(name)])
+            self.names.remove(name)
+        except:
+            pass
+
+    def killAll(self):
+        for i in range(0, len(self.names)):
+            print(self.process[i].pid)
+            os.killpg(os.getpgid(self.process[i].pid), signal.SIGTERM)
+
---
+++ run.py
@@ -0,0 +1,25 @@
+import Process
+
+processController = Process.Process()
+processController.add('./virtualmachine.sh 5900 01 machine-1  4444', 'VirtualMachine_1', '.', 'True')
+processController.add('python socket_server.py 6000 5900 4444', 'Engine_1', '.')
+
+while True:
+    command = input().split(' ')
+
+    if command[0] == 'list':
+        processes = processController.listProcess()
+        for i in range(0,len(processes)):
+            print('{}. {} - {}'.format(i, processes[i], processController.getpid(processes[i])))
+            
+    if command[0] == 'kill':
+        processes = processController.listProcess()
+        print('Process {} killed'.format(processes[int(command[1])]))
+        processController.kill(processes[int(command[1])])
+
+    if command[0] == 'add':
+        command_name = command[1]
+        command_pwd = command[2]
+        command_string = " ".join(command[3:])
+        processController.add(command_string, command_name, command_pwd)
+        print('Process added {} : {}'.format(command_name, command_string))
