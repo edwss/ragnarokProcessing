6b324b7fba6c6de5b95adcd06f806eda82498faa
Eduardo Ferreira <eduardo.sni.ferreira@gmail.com>
2020-04-24 14:21:55 -0300

    Deleted unnecessary files

--- run.sh
+++
@@ -1,6 +0,0 @@
-exec ./virtualmachine.sh 5900 01 machine-1  4444 &
-exec ./virtualmachine.sh 5901 02 merchant  4445 &
-
-source env/bin/activate
-exec python socket_server.py 4300 5900 4444 &
-exec python socket_server.py 4301 5901 4445 &
